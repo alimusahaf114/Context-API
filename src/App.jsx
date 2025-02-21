4c4a19ad5f0ecd2a44fa8b033b00ae747b657d7a
alimusahaf114 <musahafali43@gmail.com>
2025-02-22 03:16:26 +0500

    Added some new concept to work with useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,48 @@ import AppName from "./Components/AppName";
 import AddTodo from "./Components/AddTodo";
 import TodoItems from "./Components/TodoItems";
 import "./App.css";
-import { useState } from "react";
+import { useReducer } from "react";
 import WelcomeMsg from "./Components/WelcomeMsg";
 import { TodoItemsContext } from "./Store/todo-items-store";
-function App() {
-  const [todoItems, setTodoItems] = useState([]);
 
-  const addNewItem = (itemName, itemDate) => {
-    setTodoItems([{ name: itemName, duedate: itemDate }]);
+const todoItemsReducer = (currTodoItems, action) => {
+  let newTodoItems = currTodoItems;
+  if (action.type === "NEW_ITEM") {
+    newTodoItems = [
+      ...currTodoItems,
+      { name: action.payload.itemName, duedate: action.payload.itemDueDate },
+    ];
+  } else if (action.type === "DELETE_ITEM") {
+    newTodoItems = currTodoItems.filter(
+      (item) => item.name !== action.payload.todoItemName
+    );
+  }
+  return newTodoItems;
+};
+function App() {
+  const [todoItems, dispatchTodoItems] = useReducer(todoItemsReducer, []);
+
+  const addNewItem = (itemName, itemDueDate) => {
+    const newItemAction = {
+      type: "NEW_ITEM",
+      payload: {
+        itemName,
+        itemDueDate,
+      },
+    };
+    dispatchTodoItems(newItemAction);
   };
 
   const deleteItem = (todoItemName) => {
-    const newItems = todoItems.filter((item) => item.name !== todoItemName);
-    setTodoItems(newItems);
+    const DeleteItemAction = {
+      type: "DELETE_ITEM",
+      payload: {
+        todoItemName,
+      },
+    };
+    dispatchTodoItems(DeleteItemAction);
+    /*
+     */
   };
   return (
     <TodoItemsContext.Provider
